0782041904ad53d5e497fd83ca289b650cff8691
gryan12 <ga.jr@outlook.com>
2020-09-30 02:18:35 +0100

    validation

--- apps/core/views.py
+++ apps/core/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
 from django.db import IntegrityError
 from django.contrib.auth import login 
+
 
 from .models import validate_user
 
@@ -16,19 +18,25 @@ def registeruser(request):
     elif request.method == 'POST':
         ##validate first
 
-
         if not validate_user(request.POST['username'],request.POST['password1'], request.POST['password2']):
             return render(request, 'core/registeruser.html', {'form':UserCreationForm(), 'error':'Passwords do not match'})
-
 
         try:
             user = User.objects.create_user(request.POST['username'], password=request.POST['password1'])
             user.save()
             login(request, user)
             return redirect('current')
-
         except IntegrityError: 
             return render(request, 'core/registeruser.html', {'form':UserCreationForm(), 'error': 'User already registered'})
+
+def login_user(request):
+    if request.method == "GET":
+        context = {'form':AuthenticationForm()}
+        return render(request, 'core/loginuser.html', context)
+
+    elif request.method == "POST":
+        user = authenticate(request, username=request.POST['username'], pasword=request.POST['password'])
+
 
 
 def current(request):
