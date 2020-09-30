7037ce9946b7afc9304246b90819193b3c708d00
gryan12 <ga.jr@outlook.com>
2020-09-30 03:19:11 +0100

    prog

--- apps/core/models.py
+++ apps/core/models.py
@@ -1,10 +1,24 @@
 import regex as re
 from django.db import models
+from django.contrib.auth.models import User
 import logging
 import os
 import hashlib
 
 # Create your models here.
+
+class Todo(models.Model):
+    title = models.CharField(max_length=100)
+    memo = models.TextField(blank=True)
+    created = models.DateTimeField(auto_now_add=True)
+    datecompleted = models.DateTimeField(null=True, blank=True)
+    important = models.BooleanField(default=False)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+
 
 def validate_user(username, p1, p2, email=None):
     if p1 != p2:
